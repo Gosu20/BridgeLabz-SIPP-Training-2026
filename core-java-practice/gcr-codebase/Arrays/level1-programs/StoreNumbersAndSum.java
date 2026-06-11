d0c4d04d38002dd7f3bfc057b1150d5dcd870fec
Ananya <workwithananya20@gmail.com>
2026-06-11 11:00:37 +0530

    [Ananya] Add: Arrays Level 1 programs

---
+++ core-java-practice/gcr-codebase/Arrays/level1-programs/StoreNumbersAndSum.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class StoreNumbersAndSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[] numbers = new double[10];
+        double total = 0;
+        int index = 0;
+
+        while (true) {
+            double value = input.nextDouble();
+
+            if (value <= 0 || index == 10) {
+                break;
+            }
+
+            numbers[index] = value;
+            index++;
+        }
+
+        for (int i = 0; i < index; i++) {
+            System.out.println(numbers[i]);
+            total += numbers[i];
+        }
+
+        System.out.println("Total = " + total);
+
+        input.close();
+    }
+}
