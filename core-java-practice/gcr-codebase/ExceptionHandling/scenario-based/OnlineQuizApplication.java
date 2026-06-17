397fc50a2f4eb3568b1dd93f27355e3dce66c7ef
Ananya <workwithananya20@gmail.com>
2026-06-17 09:06:14 +0530

    [Ananya] Added exception handling scenarios and LeetCode solutions

---
+++ core-java-practice/gcr-codebase/ExceptionHandling/scenario-based/OnlineQuizApplication.java
@@ -0,0 +1,34 @@
+public class OnlineQuizApplication {
+
+    public static void main(String[] args) {
+
+        String[] answers =
+                {"A", "B", "C", "D"};
+
+        for (int i = 0; i <= 5; i++) {
+
+            try {
+
+                if (answers[i].equals("A")) {
+
+                    System.out.println(
+                            "Correct at index " + i);
+                }
+
+            } catch (
+                    ArrayIndexOutOfBoundsException e) {
+
+                System.out.println(
+                        "Invalid answer index: "
+                                + i);
+
+            } catch (
+                    NullPointerException e) {
+
+                System.out.println(
+                        "Answer is null at index: "
+                                + i);
+            }
+        }
+    }
+}
---
+++ core-java-practice/gcr-codebase/ExceptionHandling/scenario-based/SqrtX69.java
@@ -0,0 +1,26 @@
+public class SqrtX69 {
+
+    public int mySqrt(int x) {
+
+        long left = 0;
+        long right = x;
+
+        while (left <= right) {
+
+            long mid = left +
+                    (right - left) / 2;
+
+            if (mid * mid == x) {
+                return (int) mid;
+            }
+
+            if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) right;
+    }
+}
